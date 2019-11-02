2defffefc326fa58803cf09074e6a701b299bc47
Ankit Deogharia <ankit.deogharia@gmail.com>
2019-11-02 17:45:18 -0400

    Updated routes for pins

--- app.py
+++ app.py
@@ -7,16 +7,28 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 db = SQLAlchemy(app)
+db.create_all()
 ma = Marshmallow(app)
 
 @app.route('/')
-def hello():
-    db.create_all()
-    tag = Tag(name="mytag", color=69)
-    tag2 = Tag(name="othertag", color=70)
-    db.session.add(tag)
+def foo():
+    p1 = Pin(title="foo", latitude=40, longitude=82)
+    p2 = Pin(title="bar", latitude=82, longitude=40)
+    db.session.add(p1)
+    db.session.add(p2)
     db.session.commit()
-    return {'tags': [tag_schema.dump(t) for t in tag.query.all()]}
+    return 'bar'
+
+@app.route('/pins')
+def get_pins():
+    return {
+        'pins': [pin_schema.dump(p) for p in Pin.query.all()]
+    }
+
+@app.route('/pins/<id>')
+def get_pin(id):
+    return pin_schema.dump(
+        Pin.query.filter_by(id=id).first())
 
 if __name__ == '__main__':
     app.run()
